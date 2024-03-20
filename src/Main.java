6674c4b54dd5c17b77cbfa1cb53d83759b5219b0
Vasechko-M <fornewstudies@gmail.com>
2024-03-20 16:36:08 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void printLeapYear(int year) {
+        if (year>1584 && (year%4 == 0 || year%400 == 0) && year%100 != 0) {
+            System.out.println(year + " год является високосным");
+        }else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+    public static void main(String[] args) {
+            printLeapYear(2023);
+    }
+}
